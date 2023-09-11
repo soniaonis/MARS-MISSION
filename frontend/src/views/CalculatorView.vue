7d4a4ea4bcf86419611c595a766890f8b37d5cdd
Sonia <soniaonis@gmail.com>
2023-09-11 11:27:37 +0200

    frontend structure

---
+++ frontend/src/views/CalculatorView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CalculatorDisplay from '../components/CalculatorDisplay.vue';
+import CalculatorButtons from '../components/CalculatorButtons.vue';
+</script>
+
+<template>
+    <CalculatorDisplay/>
+    <CalculatorButtons />
+</template>
+
+<script setup>
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CalculatorView from './CalculatorView.vue';
+</script>
+
+<template>
+  <main>
+    <CalculatorView />
+  </main>
+</template>
